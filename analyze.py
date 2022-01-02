233400816e82a78994ce589c708f9c48f116400f
Grant <grantgariepy@protonmail.com>
2022-01-01 18:02:35 -0700

    now returns csv of top 250 and poster URLs. Need to make another loop to scrape higher res .jpg's

--- analyze.py
+++ analyze.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 
 movieNames = [] # Movie Names
-years = [] # Release Years
-genres = [] # Movie Genres
-imdbRatings = [] # IMDB Ratings
-poster = [] # Movie Poster
+# years = [] # Release Years
+# genres = [] # Movie Genres
+# imdbRatings = [] # IMDB Ratings
+imdbPoster = [] # Movie Poster
 
 # For monitoring request frequency
 startTime = time.time()
@@ -17,9 +17,8 @@ reqNum = 0
 
 print("Fetching Webpages...")
 
-for i in range(1, 201 + 1, 50):
-	url = ('https://www.imdb.com/search/title/?groups=top_250&sort=user_rating,desc&start=' \
-		 + str(i) + '&ref_=adv_nxt')
+for i in range(1, 50 + 1, 50):
+	url = ('https://www.imdb.com/chart/top/')
 
 	# Make a get request
 	try:
@@ -35,49 +34,62 @@ for i in range(1, 201 + 1, 50):
 	time.sleep(randint(1,4))
 
 	# Monitor the request frequency
-	reqNum += 1
+	reqNum += 0
 	elapsedTime = time.time() - startTime
-	print(f"Request: {reqNum}; Frequency: {reqNum/elapsedTime:.6f} requests/s")
+	print(f"Requesting...")
 
 	# Parse the HTML Contents
 	imdbSoup = bs(response.text, 'lxml')
-	movieContainers = imdbSoup.find_all('div', \
-		class_ = 'lister-item mode-advanced')
+	movieContainers = imdbSoup.find_all('td', \
+		class_ = 'titleColumn',)
 
 	for container in movieContainers:
 
 		# Movie Name
-		name = container.h3.a.text
+		name = container.a.text
 		movieNames.append(name)
+		
 
-		# Release Year
-		year = container.h3.find('span', class_ = 'lister-item-year').text
-		years.append(int(year[-5:-1]))
+		# # Release Year
+		# year = container.h3.find('span', class_ = 'lister-item-year').text
+		# years.append(int(year[-5:-1]))
 
-		# Movie Genre
-		genre = container.p.find('span', class_ = 'genre') \
-			.text.strip('\n').strip()
-		genres.append(genre.split(', '))
+		# # Movie Genre
+		# genre = container.p.find('span', class_ = 'genre') \
+		# 	.text.strip('\n').strip()
+		# genres.append(genre.split(', '))
 
-		# IMDB Rating
-		rating = container.strong.text
-		imdbRatings.append(float(rating))
+		# # IMDB Rating
+		# rating = container.strong.text
+		# imdbRatings.append(float(rating))
+
+		# # IMDB Poster
+		# poster = container.a.img.find('span', class_= 'lister-item-image float-left')
+		# imdbPoster.append(poster)
+
+# NEED TO USE THE LOOP THAT SCRAPES THE TOP 250 PAGE TO SCRAPE THE DIRECT IMDB PAGE OF EACH MOVIE AND 
+# GRAB THE URL (IMG / SRC) OF THE POSTER.		
+	
+	posterContainers = imdbSoup.find_all('td', \
+		class_ = 'posterColumn',)
+	
+	for container in posterContainers:
 
 		# IMDB Poster
-		poster = container.a.find('span', class_= 'lister-item-image')
-		
+		poster = container.find('img')
+		imdbPoster.append(poster['src'])
 
 print()
 
 # Create DataFrame of Movie Data
 movieRatings = pd.DataFrame({
 'Movie': movieNames,
-'Year': years,
-'Genre': genres,
-'IMDB Rating': imdbRatings,
-'Poster': poster,
+# 'Year': years,
+# 'Genre': genres,
+# 'IMDB Rating': imdbRatings,
+'Poster': imdbPoster,
 })
 
 # Export data to .csv
-movieRatings.to_csv('movieRatings.csv', encoding = 'utf-8', index = False)
+movieRatings.to_csv('movieRatings.csv', encoding = 'utf-8', index = True)
 print("Data Exported to movieRatings.csv")
